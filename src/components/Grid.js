747ca1502c1171a4fe36b52ffd10d01700cfedc3
Aaron Sanchez <saarong12@gmail.com>
2023-01-24 13:00:29 -0600

    Grid responds to click events

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,18 +1,21 @@
 const Grid = () => {
+  const handleClick = () => {
+    console.log('clicked')
+  }
   return (
     <>
       <section className="grid-container">
         <div className="instrument">
           <p>Kick</p>
         </div>
-        <div className="step"></div>
-        <div className="step"></div>
-        <div className="step"></div>
-        <div className="step"></div>
-        <div className="step"></div>
-        <div className="step"></div>
-        <div className="step"></div>
-        <div className="step"></div>
+        <div className="step" onClick={handleClick}></div>
+        <div className="step" onClick={handleClick}></div>
+        <div className="step" onClick={handleClick}></div>
+        <div className="step" onClick={handleClick}></div>
+        <div className="step" onClick={handleClick}></div>
+        <div className="step" onClick={handleClick}></div>
+        <div className="step" onClick={handleClick}></div>
+        <div className="step" onClick={handleClick}></div>
       </section>
     </>
   )
