73e5bdc8bab5511cda3c1fa36af522db78144c1e
Aaron Sanchez <saarong12@gmail.com>
2024-01-19 22:17:35 -0600

    Adjusted the width and gap of the grid steps

--- index.js
+++ index.js
@@ -3,11 +3,17 @@ let root = document.querySelector('.root')
 // GRID
 const stepCount = 16
 
-const gridContainer = document.createElement('div')
-gridContainer.classList.add('grid-container')
+const controlsContainer = document.createElement('div')
+controlsContainer.classList.add('controls-container')
 
-root.appendChild(gridContainer)
+root.appendChild(controlsContainer)
 
+const gridContainter = document.createElement('div')
+gridContainter.classList.add('grid-container')
+
+controlsContainer.appendChild(gridContainter)
+
+// Click handler callback function
 const toggleBeat = (target) => {
   target.classList.toggle('active')
 }
@@ -17,5 +23,5 @@ for (let i = 1; i <= stepCount; i++) {
   step.classList.add('step')
   step.classList.add(`count-${i}`)
   step.addEventListener('click', () => toggleBeat(step))
-  gridContainer.appendChild(step)
+  gridContainter.appendChild(step)
 }
