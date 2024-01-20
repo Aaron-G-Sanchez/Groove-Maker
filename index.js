3e8561846246603d87e484c288ff7756d275238a
Aaron Sanchez <saarong12@gmail.com>
2024-01-19 20:39:45 -0600

    Added grid layout as well as toggle for class list for activating a beat

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 // GRID
-let root = document.querySelector('#root')
+let root = document.querySelector('.root')
+
+const stepCount = 16
 
 const gridContainer = document.createElement('div')
 gridContainer.classList.add('grid-container')
 
 root.appendChild(gridContainer)
+
+const toggleBeat = (target) => {
+  target.classList.toggle('active')
+}
+
+for (let i = 1; i <= stepCount; i++) {
+  let step = document.createElement('div')
+  step.classList.add('step')
+  step.classList.add(`count-${i}`)
+  step.addEventListener('click', () => toggleBeat(step))
+  gridContainer.appendChild(step)
+}
